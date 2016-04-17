718dd59ab0959f9f7048bc1184ba7b91753fcf0f
Koichi Murase <myoga.murase@gmail.com>
2016-04-18 03:48:24 +0900

    rktest: add midpoint, Cooper-Verner 8th.

--- src/rktest.cpp
+++ src/rktest.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <mwg/except.h>
 #include <mwg/xprintf.h>
+#include "rk16.h"
 
 //-----------------------------------------------------------------------------
 // 方程式
@@ -23,83 +24,6 @@ double exactSolution(double t){
 
 //-----------------------------------------------------------------------------
 // RK integrator
-
-namespace kashiwa{
-namespace rk16{
-
-struct working_buffer{
-  std::vector<double> data;
-public:
-  void ensure(std::size_t minimalSize){
-    if(data.size()<minimalSize)
-      data.resize(minimalSize);
-  }
-
-  double      * ptr()      {return &data[0];}
-  double const* ptr() const{return &data[0];}
-};
-
-// RK4 (classical Runge-Kutta method)
-
-struct rk4_integrator{
-  mutable working_buffer buffer;
-
-  template<typename F>
-  void operator()(double& time,double* value,std::size_t size,F const& f,double h) const{
-    buffer.ensure(3*size);
-    double* knode = buffer.ptr();
-    double* xnode = buffer.ptr()+size;
-    double* delta = buffer.ptr()+size*2;
-
-    // k1
-    f(knode,time,value);
-    for(std::size_t i=0;i<size;i++){
-      delta[i] = (1.0/6.0)*h*knode[i];
-      xnode[i] = value[i] + 0.5*h*knode[i];
-    }
-
-    // k2
-    f(knode,time+0.5*h,xnode);
-    for(std::size_t i=0;i<size;i++){
-      delta[i] += (2.0/6.0)*h*knode[i];
-      xnode[i] = value[i] + 0.5*h*knode[i];
-    }
-
-    // k3
-    f(knode,time+0.5*h,xnode);
-    for(std::size_t i=0;i<size;i++){
-      delta[i] += (2.0/6.0)*h*knode[i];
-      xnode[i] = value[i] + h*knode[i];
-    }
-
-    // k4
-    f(knode,time+h,xnode);
-    for(std::size_t i=0;i<size;i++){
-      double const a = delta[i] + (1.0/6.0)*h*knode[i];
-      value[i] += a;
-    }
-
-    time+=h;
-  }
-};
-
-struct euler_integrator{
-  mutable working_buffer buffer;
-
-  template<typename F>
-  void operator()(double& time,double* value,std::size_t size,F const& f,double h) const{
-    buffer.ensure(size);
-    double* knode = buffer.ptr();
-    f(knode,time,value);
-    for(std::size_t i=0;i<size;i++)
-      value[i] += h*knode[i];
-
-    time+=h;
-  }
-};
-
-}
-}
 
 //-----------------------------------------------------------------------------
 
@@ -124,14 +48,24 @@ void test_method(std::FILE* file,Integrator const& integ,int nstage){
 int main(){
   std::FILE* file;
 
+  mwg_printd("euler");
+  file = std::fopen("out/rk/rkeuler.txt","wb");
+  test_method(file,kashiwa::rk16::euler_integrator(), 1);
+  std::fclose(file);
+
+  mwg_printd("mid");
+  file = std::fopen("out/rk/rkmid.txt","wb");
+  test_method(file,kashiwa::rk16::midpoint_integrator(), 2);
+  std::fclose(file);
+
   mwg_printd("rk4");
   file = std::fopen("out/rk/rkrk4.txt","wb");
   test_method(file,kashiwa::rk16::rk4_integrator(), 4);
   std::fclose(file);
 
-  mwg_printd("euler");
-  file = std::fopen("out/rk/rkeuler.txt","wb");
-  test_method(file,kashiwa::rk16::euler_integrator(), 1);
+  mwg_printd("cv8");
+  file = std::fopen("out/rk/rkcv8.txt","wb");
+  test_method(file,kashiwa::rk16::cooper_verner8_integrator(), 11);
   std::fclose(file);
 
   return 0;
--- src/rktest.gp
+++ src/rktest.gp
@@ -10,4 +10,8 @@ set format x "10^{%L}"
 set format y "10^{%L}"
 set xlabel 'number of eval(\dot x)'
 set ylabel 'abs(error)'
-plot "out/rk/rkeuler.txt" u 1:(abs($5)) title 'Euler', "out/rk/rkrk4.txt" u 1:(abs($5)) title 'RK4'
+plot \
+  "out/rk/rkeuler.txt" u 1:(abs($5)) title 'Euler', \
+  "out/rk/rkmid.txt" u 1:(abs($5)) title 'midpoint', \
+  "out/rk/rkrk4.txt" u 1:(abs($5)) title 'RK4', \
+  "out/rk/rkcv8.txt" u 1:(abs($5)) title 'Cooper Verner 8'
