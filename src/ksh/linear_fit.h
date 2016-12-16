2e17dad7535cc22cd191eddfc97e1925ec58f5b4
Koichi Murase <myoga.murase@gmail.com>
2016-12-16 14:31:01 +0900

    add ksh/linear_fit.h

---
+++ src/ksh/linear_fit.h
@@ -0,0 +1,32 @@
+// -*- mode: C++; coding: utf-8 -*-
+#ifndef KASHIWA_LINEAR_FIT_H
+#define KASHIWA_LINEAR_FIT_H
+#include <cstdlib>
+#include <numeric>
+#include "buffer.h"
+#include "linear_lu.h"
+namespace kashiwa {
+
+  void linear_fitting(int NParam, double* param, int NData, double const* data, double const* basis, working_buffer& buffer) {
+    buffer.ensure<double>(NParam + NParam * NParam);
+    double* contra     = buffer.ptr<double>();
+    double* covariance = contra + NParam;
+
+    for (int i = 0; i < NParam; i++) {
+      double* const fi = basis + i * NData;
+      contra[i] = std::inner_product(fi, fi + NData, data, 0.0);
+      for (int j = 0; j <= i; j++) {
+        double* const fj = basis + j * NData;
+        double const value = std::inner_product(fi, fi + NData, fj, 0.0);
+        covariance[i * NParam + j] = value;
+        covariance[j * NParam + i] = value;
+      }
+    }
+
+    working_buffer buffer2;
+    kashiwa::solve_linear_equation_lu(NParam, contra, covariance, contra, buffer2, covariance);
+  }
+
+}
+
+#endif
