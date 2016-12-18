29597e66d5155f7e0e263602e4962e676eada207
Koichi Murase <myoga.murase@gmail.com>
2016-12-19 05:42:47 +0900

    ksh/linear_fit: fix a bug that const was missing for the local variables

--- src/ksh/linear_fit.h
+++ src/ksh/linear_fit.h
@@ -7,16 +7,16 @@
 #include "linear_lu.h"
 namespace kashiwa {
 
-  void linear_fitting(int NParam, double* param, int NData, double const* data, double const* basis, working_buffer& buffer) {
+  void linear_fit(int NParam, double* param, int NData, double const* data, double const* basis, working_buffer& buffer) {
     buffer.ensure<double>(NParam + NParam * NParam);
-    double* contra     = buffer.ptr<double>();
-    double* covariance = contra + NParam;
+    double* const contra     = buffer.ptr<double>();
+    double* const covariance = contra + NParam;
 
     for (int i = 0; i < NParam; i++) {
-      double* const fi = basis + i * NData;
+      double const* const fi = basis + i * NData;
       contra[i] = std::inner_product(fi, fi + NData, data, 0.0);
       for (int j = 0; j <= i; j++) {
-        double* const fj = basis + j * NData;
+        double const* const fj = basis + j * NData;
         double const value = std::inner_product(fi, fi + NData, fj, 0.0);
         covariance[i * NParam + j] = value;
         covariance[j * NParam + i] = value;
