f8575daa4f7ee510563056f02c1faf9b483ee388
Koichi Murase <myoga.murase@gmail.com>
2017-02-04 13:27:03 +0900

    rename test/multi_precision.cpp to ksh/big_integer.h, test/big_integer.cpp

---
+++ src/test/big_integer.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+template<typename S, typename C, C M>
+void dump(kashiwa::big_integer<S, C, M> const& value) {
+  std::printf("sign = %d, data = [ ", value.sign);
+  for (std::size_t i = value.data.size(); i--; ) {
+    std::printf("%llx", (unsigned long long) value.data[i]);
+    if (i != 0) std::printf(", ");
+  }
+  std::printf(" ]\n");
+}
+
+void test1() {
+  kashiwa::big_integer<std::uint32_t, std::uint64_t, 0x100> a = 1234;
+  dump(a);
+  for (int i = 0; i <= 100; i++) {
+    a += a;
+    a++;
+    dump(a);
+  }
+}
+
+int main() {
+  test1();
+  return 0;
+}
